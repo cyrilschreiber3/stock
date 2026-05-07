48dede1a3ad446c8440568d1e5f634553903c681
Cyril Schreiber <contact@cyrilschreiber.ch>
2026-05-07 18:11:13 +0200

    feat(database): add UpdateProduct query

--- database/queries/products.sql
+++ database/queries/products.sql
@@ -9,5 +9,11 @@ INSERT INTO products (brand, name, subtype, aliases, default_price)
 VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
 
+-- name: UpdateProduct :one
+UPDATE products
+SET brand = $2, name = $3, subtype = $4, aliases = $5, default_price = $6
+WHERE id = $1
+RETURNING *;
+
 -- name: DeleteProduct :execrows
 DELETE FROM products WHERE id = $1;
