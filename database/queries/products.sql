446d869d322d774f0850dcc2d8e4e35922ab258a
cyrilschreiber3 <contact@cyrilschreiber.ch>
2026-05-16 17:51:07 +0200

    feat(database): update queries for product and add for categories and suppliers

--- database/queries/products.sql
+++ database/queries/products.sql
@@ -5,15 +5,128 @@ SELECT * FROM products;
 SELECT * FROM products WHERE id = $1;
 
 -- name: CreateProduct :one
-INSERT INTO products (brand, name, subtype, aliases, default_price)
-VALUES ($1, $2, $3, $4, $5)
+INSERT INTO products (brand, name, aliases, category_id, subcategory_id, default_supplier_id, default_buy_price, default_sell_price)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 RETURNING *;
 
 -- name: UpdateProduct :one
 UPDATE products
-SET brand = $2, name = $3, subtype = $4, aliases = $5, default_price = $6
+SET brand = $2, name = $3, aliases = $4, category_id = $5, subcategory_id = $6, default_supplier_id = $7, default_buy_price = $8, default_sell_price = $9
 WHERE id = $1
 RETURNING *;
 
 -- name: DeleteProduct :execrows
 DELETE FROM products WHERE id = $1;
+
+
+-- Category queries
+
+-- name: GetAllCategories :many
+SELECT * FROM categories;
+
+-- name: GetCategoryByID :one
+SELECT * FROM categories WHERE id = $1;
+
+-- name: CreateCategory :one
+INSERT INTO categories (name, description)
+VALUES ($1, $2)
+RETURNING *;
+
+-- name: UpdateCategory :one
+UPDATE categories
+SET name = $2, description = $3
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteCategory :execrows
+DELETE FROM categories WHERE id = $1;
+
+-- Subcategory queries
+
+-- name: GetAllSubcategories :many
+SELECT * FROM subcategories;
+
+-- name: GetSubcategoryByID :one
+SELECT * FROM subcategories WHERE id = $1;
+
+-- name: CreateSubcategory :one
+INSERT INTO subcategories (name, description, category_id)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: UpdateSubcategory :one
+UPDATE subcategories
+SET name = $2, description = $3, category_id = $4
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteSubcategory :execrows
+DELETE FROM subcategories WHERE id = $1;
+
+-- Supplier queries
+
+-- name: GetAllSuppliers :many
+SELECT * FROM suppliers;
+
+-- name: GetSupplierByID :one
+SELECT * FROM suppliers WHERE id = $1;
+
+-- name: CreateSupplier :one
+INSERT INTO suppliers (name, description)
+VALUES ($1, $2)
+RETURNING *;
+
+-- name: UpdateSupplier :one
+UPDATE suppliers
+SET name = $2, description = $3
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteSupplier :execrows
+DELETE FROM suppliers WHERE id = $1;
+
+-- Subcategory queries by category
+
+-- name: GetSubcategoriesByCategoryID :many
+SELECT * FROM subcategories WHERE category_id = $1;
+
+-- name: DeleteSubcategoriesByCategoryID :execrows
+DELETE FROM subcategories WHERE category_id = $1;
+
+-- Product queries by category and subcategory
+
+-- name: GetProductsByCategoryID :many
+SELECT * FROM products WHERE category_id = $1;
+
+-- name: GetProductsBySubcategoryID :many
+SELECT * FROM products WHERE subcategory_id = $1;
+
+-- Product queries by supplier
+
+-- name: GetProductsBySupplierID :many
+SELECT * FROM products WHERE default_supplier_id = $1;
+
+-- Product with all details
+
+-- name: GetAllProductWithDetails :many
+SELECT
+    p.*,
+    c.name AS category_name,
+    sc.name AS subcategory_name,
+    s.name AS default_supplier_name
+FROM products p
+INNER JOIN categories c ON p.category_id = c.id
+INNER JOIN subcategories sc ON p.subcategory_id = sc.id
+INNER JOIN suppliers s ON p.default_supplier_id = s.id;
+
+-- name: GetProductWithDetailsByID :one
+SELECT
+    p.*,
+    c.name AS category_name,
+    sc.name AS subcategory_name,
+    s.name AS default_supplier_name
+FROM products p
+INNER JOIN categories c ON p.category_id = c.id
+INNER JOIN subcategories sc ON p.subcategory_id = sc.id
+INNER JOIN suppliers s ON p.default_supplier_id = s.id
+WHERE p.id = $1;
