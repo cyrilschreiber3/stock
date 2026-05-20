42ecc2d4e7a3e258b2e21bda5ed6979e41ac20a8
cyrilschreiber3 <contact@cyrilschreiber.ch>
2026-05-20 22:18:52 +0200

    fix(routes): update routes to be more expressive without the http method

--- routes/routes.go
+++ routes/routes.go
@@ -14,9 +14,10 @@ func SetupRoutes(router *gin.Engine) {
 		productGroup.GET("", handlers.HandleGetProducts())
 		productGroup.GET("/create", handlers.HandleShowCreateProductForm())
 		productGroup.POST("/create", handlers.HandleCreateProduct())
-		productGroup.GET("/:id", handlers.HandleShowUpdateProductForm())
-		productGroup.PUT("/:id", handlers.HandleUpdateProduct())
-		productGroup.DELETE("/:id", handlers.HandleDeleteProduct())
+		productGroup.GET("/:id/show")
+		productGroup.GET("/:id/edit", handlers.HandleShowUpdateProductForm())
+		productGroup.PUT("/:id/update", handlers.HandleUpdateProduct())
+		productGroup.DELETE("/:id/delete", handlers.HandleDeleteProduct())
 	}
 
 	categoryGroup := router.Group("/categories")
@@ -25,9 +26,10 @@ func SetupRoutes(router *gin.Engine) {
 		categoryGroup.GET("/options", handlers.HandleGetCategoryOptions())
 		categoryGroup.GET("/create")
 		categoryGroup.POST("/create")
-		categoryGroup.GET("/:id")
-		categoryGroup.PUT("/:id")
-		categoryGroup.DELETE("/:id")
+		categoryGroup.GET("/:id/show")
+		categoryGroup.GET("/:id/edit")
+		categoryGroup.PUT("/:id/update")
+		categoryGroup.DELETE("/:id/delete")
 
 		subcategoryGroup := categoryGroup.Group("/:id/subcategories")
 		{
@@ -35,9 +37,10 @@ func SetupRoutes(router *gin.Engine) {
 			subcategoryGroup.GET("/options", handlers.HandleGetSubcategoryOptions())
 			subcategoryGroup.GET("/create")
 			subcategoryGroup.POST("/create")
-			subcategoryGroup.GET("/:id")
-			subcategoryGroup.PUT("/:id")
-			subcategoryGroup.DELETE("/:id")
+			subcategoryGroup.GET("/:id/show")
+			subcategoryGroup.GET("/:id/edit")
+			subcategoryGroup.PUT("/:id/update")
+			subcategoryGroup.DELETE("/:id/delete")
 		}
 	}
 
@@ -45,11 +48,12 @@ func SetupRoutes(router *gin.Engine) {
 	{
 		supplierGroup.GET("", handlers.HandleGetSuppliers())
 		supplierGroup.GET("/options", handlers.HandleGetSupplierOptions())
-		supplierGroup.GET("/create")
-		supplierGroup.POST("/create")
-		supplierGroup.GET("/:id")
-		supplierGroup.PUT("/:id")
-		supplierGroup.DELETE("/:id")
+		supplierGroup.GET("/create", handlers.HandleShowCreateSupplierForm())
+		supplierGroup.POST("/create", handlers.HandleCreateSupplier())
+		supplierGroup.GET("/:id/show")
+		supplierGroup.GET("/:id/edit", handlers.HandleShowUpdateSupplierForm())
+		supplierGroup.PUT("/:id/update", handlers.HandleUpdateSupplier())
+		supplierGroup.DELETE("/:id/delete", handlers.HandleDeleteSupplier())
 	}
 
 	router.GET("/", handlers.Index())
